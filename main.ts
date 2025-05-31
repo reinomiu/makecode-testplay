055f1da3bfd05a1c326afe7e5d19dc65a0e09a38
reinomiu <fl25a006@oecu.jp>
2025-05-31 10:58:45 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+    basic.showLeds(`
+        # # # . .
+        # # # . .
+        # # . . .
+        # # # . .
+        # # # . .
+        `)
+    basic.pause(100)
+    basic.clearScreen()
+    basic.showString("“TOSHIBA saiko!! 1048 315!”")
+    basic.clearScreen()
+})
